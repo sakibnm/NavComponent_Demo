01d6aa9589e8867f6df155c09d120e42cfac5e15
sakib <sakibnm@gmail.com>
2023-02-03 23:34:54 -0500

    updated with add

--- app/src/main/java/space/sakibnm/navcomponent_demo/FragmentHome.java
+++ app/src/main/java/space/sakibnm/navcomponent_demo/FragmentHome.java
@@ -3,55 +3,30 @@ package space.sakibnm.navcomponent_demo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentHome#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FragmentHome extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText editTextFirstInt, editTextSecondInt;
+    private Button buttonAdd, buttonMultiply;
+    private int firstInt, secondInt;
 
     public FragmentHome() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentHome.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentHome newInstance(String param1, String param2) {
-        FragmentHome fragment = new FragmentHome();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
+        @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -59,6 +34,30 @@ public class FragmentHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        editTextFirstInt = rootView.findViewById(R.id.editTextFirstInt);
+        editTextSecondInt = rootView.findViewById(R.id.editTextSecondInt);
+        buttonAdd = rootView.findViewById(R.id.buttonAdd);
+        buttonMultiply = rootView.findViewById(R.id.buttonMultiply);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstInt = Integer.parseInt(String.valueOf(editTextFirstInt.getText()));
+                secondInt = Integer.parseInt(String.valueOf(editTextSecondInt.getText()));
+
+                Bundle bundle = new Bundle();
+                bundle.putInt("num1", firstInt);
+                bundle.putInt("num2", secondInt);
+//
+                Navigation.findNavController(rootView)
+                        .navigate(R.id.action_fragmentHome_to_fragmentAdd,bundle);
+
+            }
+        });
+
+
+        return rootView;
     }
 }
--- app/src/main/java/space/sakibnm/navcomponent_demo/FragmentMultiply.java
+++ app/src/main/java/space/sakibnm/navcomponent_demo/FragmentMultiply.java
@@ -8,50 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentMultiply#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FragmentMultiply extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public FragmentMultiply() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentMultiply.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentMultiply newInstance(String param1, String param2) {
-        FragmentMultiply fragment = new FragmentMultiply();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -59,6 +27,7 @@ public class FragmentMultiply extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_multiply, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_multiply, container, false);
+        return rootView;
     }
 }
